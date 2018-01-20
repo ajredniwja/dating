22392bf702132b247b267fe7eb4c59e5cbcfb58e
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-19 20:37:28 -0500

    Added basic layout and images

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ajwinder
- * Date: 1/19/18
- * Time: 5:32 AM
- */
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base Class
+$f3 = Base :: instance();
+
+//Set debug level
+//will take care of php errors as well which gives 500 error
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function ()
+{
+    $view = new View;
+    echo $view->render
+    ('pages/home.html');
+    //echo '<h1>Hello, world!</h1>';
+}
+);
+
+//Run fat free
+$f3->run();
