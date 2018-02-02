047cf6210cc266a9890b79c9d12cdc8567b7a59f
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-02-02 10:13:59 -0500

    session array and summary page left

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
 //Require the autoload file
 require_once('vendor/autoload.php');
+session_start();
 
 //Create an instance of the Base Class
 $f3 = Base :: instance();
@@ -9,14 +12,99 @@ $f3 = Base :: instance();
 //will take care of php errors as well which gives 500 error
 $f3->set('DEBUG', 3);
 
+$f3->set('indoor', array('tv', 'movies', 'cooking', 'board games', 'puzzles', 'reading', 'playing cards', 'video games'));
+$f3->set('outdoor', array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing'));
+$f3->set('state',array('Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida',
+    'Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota',
+    'Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota',
+    'Ohio','Oklahoma','Oregon','Pennsylvania', 'Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont',
+    'Virginia','Washington','West Virginia','Wisconsin','Wyoming'));
+
 //Define a default route
-$f3->route('GET /', function ()
+$f3->route('GET /', function($f3)
 {
-    $view = new View;
-    echo $view->render
-    ('pages/home.html');
-}
-);
+    $template = new Template();
+    echo $template->render('pages/home.html');
+
+});
+
+$f3->route('GET|POST /personalInfo', function($f3)
+{
+    if (isset($_POST['submit']))
+    {
+        print_r($_POST);
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        $pnumber = $_POST['pnumber'];
+
+        require ('model/validate.php');
+
+        $f3->set('fname',$fname);
+        $f3->set('lname',$lname);
+        $f3->set('age',$age);
+        $f3->set('gender',$gender);
+        $f3->set('pnumber',$pnumber);
+        $f3->set('success',$success);
+        $f3->set('errors', $errors);
+        print_r($errors);
+        if($success)
+        {
+            header("Location: http://asingh.greenriverdev.com/328/dating/profile");
+        }
+    }
+
+    $template = new Template();
+    echo $template->render('pages/personalInformation.html');
+
+});
+
+
+$f3->route('GET|POST /profile', function($f3)
+{
+    if (isset($_POST['submit']))
+    {
+        print_r($_POST);
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $seeking = $_POST['seeking'];
+
+        //require ('model/validate.php');
+
+        $f3->set('email',$email);
+        $f3->set('state',$state);
+        $f3->set('seeking',$seeking);
+        $f3->set('success',$success);
+        $f3->set('errors', $errors);
+        print_r($errors);
+    }
+
+    $template = new Template();
+    echo $template->render('pages/profile.html');
+
+});
+
+$f3->route('GET|POST /interests', function($f3)
+{
+    if (isset($_POST['submit']))
+    {
+        print_r($_POST);
+//        header("Location: http://asingh.greenriverdev.com/328/dating/summary");
+    }
+
+    $template = new Template();
+    echo $template->render('pages/interests.html');
+
+});
+
+$f3->route('GET|POST /summary', function($f3)
+{
+    $template = new Template();
+    echo $template->render('pages/summary.html');
+
+});
+
 
 //Run fat free
 $f3->run();
--- model/validate.php
+++ model/validate.php
@@ -1,7 +1,61 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ajwinder
- * Date: 2/2/18
- * Time: 3:02 AM
- */
+
+function validName($fname)
+{
+    return !empty($fname) && ctype_alpha($fname);
+}
+
+validName($lname);
+
+function validAge($age)
+{
+    return is_numeric($age) && ($age >= 18);
+}
+
+function validPhone($pnumber)
+{
+    //$pnumber = preg_replace("/{[-:; /", "", $pnumber);
+    return is_numeric($pnumber) && strlen($pnumber)  <= 10;
+}
+
+function validOutdoor($out, $outdoor)
+{
+    //return in_array($out, $outdoor);
+}
+
+function validIndoor($in, $indoor)
+{
+    //return in_array($in, $indoor);
+}
+
+if(!validName($fname))
+{
+    $errors['fname'] = "Please enter a valid First Name";
+}
+
+if(!validName($lname))
+{
+    $errors['lname'] = "Please enter a valid Last Name";
+}
+
+
+if(!validAge($age))
+{
+    $errors['age'] = "Please enter a valid age";
+}
+
+if(!validPhone($pnumber))
+{
+    $errors['pnumber'] = "Please enter a valid Phone Number";
+}
+
+//if(!validOutdoor($out, $outdoor))
+//{
+//    $errors['out'] = "Please select Valid activity";
+//}
+//if(!validAge($in, $indoor))
+//{
+//    $errors['in'] = "Please select Valid activity";
+//}
+
+$success = sizeof($errors) == 0;
